60c153390f083c1eac9ee67d7659c812698df617
ZhangXinJie6 <211088842@qq.com>
2016-09-23 15:57:21 +0800

    Initial commit

---
+++ app/src/main/java/landandport/se/cs/bistu/edu/cn/landandport/LandActivity.java
@@ -0,0 +1,11 @@
+package landandport.se.cs.bistu.edu.cn.landandport;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LandActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
